71c9d609f6d7a5454a186ce23ee42abc175e353b
GoddessLune <GoddessLune@proton.me>
2025-05-30 19:31:14 -0400

    Create script.js

---
+++ styles.css/script.js
@@ -0,0 +1,12 @@
+// Hypnotic effect for clicking tribute tiers
+function showTier(tier) {
+    let msg = "";
+    if (tier === "pig") {
+        msg = "You chose 🐖 Tribute Pig. Your obedience is noticed. Pay and await my next command.";
+    } else if (tier === "worshipper") {
+        msg = "You chose 💎 Loyal Worshipper. Only the devoted receive my attention. Tribute and be seen.";
+    } else if (tier === "sub") {
+        msg = "You chose 👑 Devoted Sub. Prepare for the privilege of my voice and custom ritual. Tribute now.";
+    }
+    alert(msg);
+}
