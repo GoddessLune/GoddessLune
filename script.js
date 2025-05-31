a599178086185b5860f3dc8fc34a0949fca6fd99
GoddessLune <GoddessLune@proton.me>
2025-05-30 20:53:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function showTier(tier) {
     let msg = "";
     if (tier === "pig") {
-        msg = "You chose 🐖 Tribute Pig. Your obedience is noticed. Pay and await my next command.";
+        msg = "🐖 You chose Tribute Pig. Your obedience is noticed. Pay and await my next command.";
     } else if (tier === "worshipper") {
-        msg = "You chose 💎 Loyal Worshipper. Only the devoted receive my attention. Tribute and be seen.";
+        msg = "💎 You chose Loyal Worshipper. Only the devoted receive my attention. Tribute and be seen.";
     } else if (tier === "sub") {
-        msg = "You chose 👑 Devoted Sub. Prepare for the privilege of my voice and custom ritual. Tribute now.";
+        msg = "👑 You chose Devoted Sub. Prepare for the privilege of my voice and custom ritual. Tribute now.";
     }
     alert(msg);
 }
+
