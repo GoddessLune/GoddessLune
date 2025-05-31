1447c4edc28af408d673cc223b81cf20133c783e
GoddessLune <GoddessLune@proton.me>
2025-05-31 00:17:49 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function showTier(tier) {
+    let msg = "";
+    if (tier === "pig") {
+        msg = "🐖 You chose Tribute Pig. Your obedience is noticed. Pay and await my next command.";
+    } else if (tier === "worshipper") {
+        msg = "💎 You chose Loyal Worshipper. Only the devoted receive my attention. Tribute and be seen.";
+    } else if (tier === "sub") {
+        msg = "👑 You chose Devoted Sub. Prepare for the privilege of my voice and custom ritual. Tribute now.";
+    }
+    alert(msg);
+}
