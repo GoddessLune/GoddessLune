c447a18601b7e478f66c36fa46870797ed6a23b9
GoddessLune <GoddessLune@proton.me>
2025-06-01 18:26:48 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
 function showTier(tier) {
-    let msg = "";
-    if (tier === "pig") {
-        msg = "🐖 You chose Tribute Pig. Your obedience is noticed. Pay and await my next command.";
-    } else if (tier === "worshipper") {
-        msg = "💎 You chose Loyal Worshipper. Only the devoted receive my attention. Tribute and be seen.";
-    } else if (tier === "sub") {
-        msg = "👑 You chose Devoted Sub. Prepare for the privilege of my voice and custom ritual. Tribute now.";
-    }
-    alert(msg);
+  let msg = "";
+  if (tier === "pig") {
+    msg = "🐷 Dungeon Pig: Submit and be humiliated.";
+  } else if (tier === "worshipper") {
+    msg = "💎 Dungeon Worshipper: Tribute for custom punishment.";
+  } else if (tier === "sub") {
+    msg = "👑 Dungeon Submissive: Prepare for ritual and reward.";
+  }
+  alert(msg);
 }
